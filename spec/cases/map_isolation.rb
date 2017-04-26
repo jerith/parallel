3d151d56353387caeeda44dcefb3a7b877912149
Jeremy Thurgood <firxen@gmail.com>
2017-04-26 10:17:14 +0200

    More workers for isolation test.

--- spec/cases/map_isolation.rb
+++ spec/cases/map_isolation.rb
@@ -1,7 +1,7 @@
 require './spec/cases/helper'
 
 process_diff do
-  result = Parallel.map([1,2,3,4], in_processes: 2, isolation: true) do |i|
+  result = Parallel.map([1,2,3,4], in_processes: 4, isolation: true) do |i|
     $i ||= i
   end
   puts result
